5352b119958b949f8c0777d112c0e853fcc7caf3
ampuriraelizabeth <elizabethampurira@gmail.com>
2022-10-04 22:20:13 +0300

    fetched my models sata using RESTapi  and displayed then in JYSON

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,46 @@
 from dataclasses import fields
+from pyexpat import model
 from rest_framework import serializers
-from wallet.models import Customer
+from wallet.models import Account, Customer, Wallet,Receipt,  Card, Transaction, Loan, Notification
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model= Customer
         fields=("first_name","email","age","address")
         
-    
+class WalletSerializer( serializers.ModelSerializer):
+    class Meta:
+        model= Wallet
+        fields= ("customer_id","balance","amount","status")
+        
+class AccountSerializer( serializers.ModelSerializer):
+    class Meta:
+        model= Account
+        fields= ("account_name","account_number","customer","wallet")
+        
+class CardSerializer(serializers.ModelSerializer):   
+    class Meta:
+        model= Card
+        fields= ("Cardholder_name","cardholder_number","wallet","issuer")   
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Transaction
+        fields= ("origin_account","destination_account","transaction_amount","transaction_ref")    
+        
+class LoanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Loan
+        fields = ("wallet","amount","loan_type","payment_due_date")
+
+
+class ReceiptSerializer( serializers.ModelSerializer):
+    class Meta:
+        model =   Receipt
+        fields= ("receipt_type","amount","transaction","receipt_file")    
+        
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model =  Notification
+        fields =("title","message","status","recipient")         
+                              
--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from wallet.models import Customer
-from .serializers import CustomerSerializer
+from wallet.models import Customer, Wallet,Account, Card, Transaction, Loan, Receipt, Notification
+from .serializers import CustomerSerializer, TransactionSerializer, WalletSerializer, AccountSerializer, CardSerializer,TransactionSerializer,LoanSerializer, ReceiptSerializer, NotificationSerializer
 
 # Create your views here.
 
@@ -9,4 +9,35 @@ class CustomerViewSet(viewsets.ModelViewSet):
     queryset= Customer.objects.all()
     serializer_class= CustomerSerializer
     
-     
+class WalletViewSet(viewsets.ModelViewSet): 
+    queryset = Wallet.objects.all()
+    serializer_class= WalletSerializer   
+
+class AccountViewSet(viewsets.ModelViewSet):
+    queryset= Account.objects.all()
+    serializer_class = AccountSerializer   
+    
+class CardViewSet(viewsets.ModelViewSet):
+    queryset= Card.objects.all()
+    serializer_class= CardSerializer
+    
+class TransactionViewSet( viewsets.ModelViewSet):
+    queryset= Transaction.objects.all()
+    serializer_class = TransactionSerializer 
+
+class LoanViewSet(viewsets.ModelViewSet):
+    queryset =Loan.objects.all()
+    serializer_class = LoanSerializer    
+    
+class ReceiptViewSet(viewsets.ModelViewSet):
+    queryset = Receipt.objects.all()
+    serializer_class = ReceiptSerializer
+    
+class NotificationViewSet(viewsets.ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer    
+    
+           
+    
+        
+      
